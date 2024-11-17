ced2b84f8d531669b300677c7d0f31e6b71cb312
Jaeson <jaesonmathew.22@ichat.sp.edu.sg>
2024-11-17 19:59:57 +0800

    Third release

--- Lab2.py
+++ Lab2.py
@@ -5,14 +5,25 @@ def display_main_menu():
     print(f"Your list = {int_list}")
     return(int_list)
 
-def calc_min(list):
-    return min(list)
+def calc_min(lists):
+    return min(lists)
 
-def calc_max(list):
-    return max(list)
+def calc_max(lists):
+    return max(lists)
 
-def calc_avg(list):
-    return sum(list)/len(list)
+def calc_avg(lists):
+    return sum(lists)/len(lists)
+
+def calc_median(lists):
+    sorted_list=sorted(lists)
+    if len(sorted_list)%2 ==1:
+        median_index=sorted_list//2
+        return sorted_list[median_index]
+    else:
+        mid1 = len(sorted_list)//2
+        mid2 = len(sorted_list)//2-1
+        midvalue = (sorted_list[mid1]+sorted_list[mid2])/2
+        return midvalue
 
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
@@ -23,6 +34,8 @@ def main():
     print(f"The min value in the list = {minval}")
     avgval=calc_avg(user_input)
     print(f"The average value in the list = {avgval}")
+    median = calc_median(user_input)
+    print(f"The median value = {median:.2f}")
 
 
 if __name__ == "__main__":
